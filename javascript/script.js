2d6f630abc750317a5e3611f5737703dffb0f021
Sopheary Chiv <hsopheary@yahoo.com>
2015-02-17 17:09:19 -0800

    update javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,15 @@
 
-function submission(){
-    alert("Thank you for your submission!");
+function validateForm(){
+    
+    var x = document.getElementById("text").value;
+    if(x == null || x == ""){
+        alert("Please enter text");
+    }else{
+    alert("Are you sure you want to submit: " + x + "?");
+    }
+//    if (x == null || x == "") {
+//        alert("Please enter text!");
+//    }else{
+//    alert("Thank you for your submission!");
+//    }
 }
